0e0db029edde2aee02d4e5c2ef89159026e0ed82
Hubert.Karw <hubert_karwowski@o2.pl>
2024-11-14 23:15:22 +0100

    java/pracadomowa/zaj6

--- zajecia/src/zajecia6/zadanie11.java
+++ zajecia/src/zajecia6/zadanie11.java
@@ -2,7 +2,16 @@ package zajecia6;
 
 public class zadanie11 {
     public static void main(String[] args) {
-        int sum=0;
-//        int
+        for(int i = 1; i<1000;i++){
+            int sum = 0;
+            for(int j=1;j<=i/2;j++){
+                if(i%j==0){
+                    sum=j+sum;
+                }
+            }
+            if (sum==i){
+                System.out.println(i);
+            }
+        }
     }
 }
--- zajecia/src/zajecia6/zadanie5.java
+++ zajecia/src/zajecia6/zadanie5.java
@@ -1,7 +1,18 @@
 package zajecia6;
 
 public class zadanie5 {
-    int number = 5505;
+    public static void main(String[] args) {
+        int number = 1234;
+        String numberAsString = Integer.toString(number);
+        System.out.println(numberAsString);
+        int stringLength = numberAsString.length();
+        while(stringLength>0){
+            System.out.println(numberAsString.charAt(stringLength-1));
+            stringLength--;
+        }
+    }
+
+
 //    while(){
 //
 //    }
